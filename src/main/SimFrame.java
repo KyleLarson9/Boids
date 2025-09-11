1d96aa0ae96dc06aaf8e7a090f7cd65edabf8941
Kyle Larson <kylelarson08@gmail.com>
2025-09-10 19:40:31 -0500

    Initial commit

---
+++ src/main/SimFrame.java
@@ -0,0 +1,16 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class SimFrame extends JFrame {
+
+	public SimFrame(SimPanel panel) {
+		this.add(panel);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+	}
+	
+}
