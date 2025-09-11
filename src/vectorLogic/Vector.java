600f2cfac0d85d3976d2d2f21f327a9f2c610c35
Kyle Larson <kylelarson08@gmail.com>
2025-09-10 22:11:34 -0500

    Built a simple vector class

---
+++ src/vectorLogic/Vector.java
@@ -0,0 +1,47 @@
+package vectorLogic;
+
+public class Vector {
+
+	private double x, y;
+	
+	public Vector(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double magnitude() {
+		return Math.sqrt(x*x + y*y);
+	}
+	
+	public void normalize() {
+		double magnitude = magnitude();
+		
+		if(magnitude != 0) {
+			x/=magnitude;
+			y/=magnitude;
+		}
+			
+	}
+	
+	public double getAngle() {
+		return Math.atan2(y, x); // angle from positive x axis
+	}
+	
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	
+}
